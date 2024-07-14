fab06e735a64bc7218ff86fed1b73197f0e4659a
borisHP <borisandres.guinez@gmail.com>
2024-07-14 17:47:51 -0400

    update sript.js agregando eventos en boton de formulario y doble click en parrafo para esconder

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,27 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+
+$(function(){
+    // Probando doble click en parrafo para esconder
+    $('p').dblclick(function(){
+        $(this).hide('slow')
+    })
+
+     //Mensaje enviado por boton
+     /* $('button').click(function(event){
+            alert("El Correo fue enviado correctamente");
+    }) */
+
+
+    // Mensaje enviado por submit        
+    $('#formulario').submit(function(event){
+        event.preventDefault();
+        //llamar a funcion que valida
+        console.log(event);
+        alert("Mensaje enviado Exitosamente")
+        this.submit();
+    
+    })
+})
