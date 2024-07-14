f3617bca3a03f8cb7c34f5daf3f37e4762a36663
borisHP <borisandres.guinez@gmail.com>
2024-07-14 15:47:20 -0400

    update sript.js incorporar tooltip boton formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
 /* alert("Probando js"); */
+/* Agregando tooltip a boton formulario */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
