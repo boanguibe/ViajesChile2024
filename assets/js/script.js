5dd1c6a660c66aefbd2dc477ff56c7d64517c678
borisHP <borisandres.guinez@gmail.com>
2024-07-14 18:08:17 -0400

    update js css y html para poner fondo cuando se hace scroll al nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-/* alert("Probando js"); */
+
 /* Agregando tooltip a boton formulario */
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-
+// Se agregan eventos jquery
 $(function(){
     // Probando doble click en parrafo para esconder
     $('p').dblclick(function(){
@@ -28,3 +28,15 @@ $(function(){
     
     })
 })
+
+// Se agrega la clase navbar-scroll al navbar cuando se hace scroll
+document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        var navbar = document.getElementById('navegacion');
+        if (window.scrollY > 50) { // Puedes ajustar el valor según tus necesidades
+            navbar.classList.add('navbar-scroll');
+        } else {
+            navbar.classList.remove('navbar-scroll');
+        }
+    });
+});
